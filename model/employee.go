4109e25938b8306de99e1fdb831c63b59212b376
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-06-13 21:08:12 +0900

    Fix auto increment issue

--- model/employee.go
+++ model/employee.go
@@ -3,7 +3,7 @@ package model
 import "gorm.io/gorm"
 
 type Employee struct {
-	EmployeeId string  `gorm:"primary key;auto increment" json:"employee_id"`
+	EmployeeId uint    `gorm:"primary key;autoIncrement" json:"employee_id"`
 	FirstName  string  `json:"first_name"`
 	LastName   string  `json:"last_name"`
 	Salary     float32 `json:"salary"`
